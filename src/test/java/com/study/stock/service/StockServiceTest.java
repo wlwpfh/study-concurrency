e6c98a0b711c7f9a6ce923ff9ae5e144f0ce32a6
yang Jiyoung <0112jyoung@naver.com>
2023-10-26 22:12:10 +0900

    test: race condition 발생 테스트

--- src/test/java/com/study/stock/service/StockServiceTest.java
+++ src/test/java/com/study/stock/service/StockServiceTest.java
@@ -6,6 +6,10 @@ import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 @SpringBootTest
 public class StockServiceTest {
     @Autowired
@@ -15,23 +19,45 @@ public class StockServiceTest {
     private StockRepository stockRepository;
 
     @BeforeEach
-    public void before(){
+    public void before() {
         stockRepository.saveAndFlush(new Stock(1L, 100L));
     }
 
     @AfterEach
-    public void after(){
+    public void after() {
         stockRepository.deleteAll();
     }
 
     @Test
     @DisplayName("하나의 요청이 들어온 재고 감소 테스트")
-    public void 재고_감소_태스트(){
+    public void 재고_감소_태스트() {
         stockService.decrease(1L, 1L);
 
         // 100 - 1 = 99
         Stock stock = stockRepository.findById(1L).orElseThrow();
 
         Assertions.assertEquals(99, stock.getQuantity());
+    }
+
+    @Test
+    @DisplayName("100개의 요청이 들어온 재고 감소 테스트 - race condition 발생")
+    public void 많은_요청_재고_감소_태스트() throws InterruptedException {
+        int threadCount = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    stockService.decrease(1L, 1L);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+        latch.await();
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+        Assertions.assertNotEquals(0, stock.getQuantity());
     }
 }
