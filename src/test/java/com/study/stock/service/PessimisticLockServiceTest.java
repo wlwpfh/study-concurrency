e5f9869061f25191b430968ca166b2fac13acba4
yang Jiyoung <0112jyoung@naver.com>
2023-10-29 17:41:34 +0900

    test: 비관적 락 테스트

---
+++ src/test/java/com/study/stock/service/PessimisticLockServiceTest.java
@@ -0,0 +1,52 @@
+package com.study.stock.service;
+
+import com.study.stock.domain.Stock;
+import com.study.stock.repository.StockRepository;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@SpringBootTest
+public class PessimisticLockServiceTest {
+    @Autowired
+    private PessimisticLockService stockService;
+
+    @Autowired
+    private StockRepository stockRepository;
+
+    @BeforeEach
+    public void before() {
+        stockRepository.saveAndFlush(new Stock(1L, 100L));
+    }
+
+    @AfterEach
+    public void after() {
+        stockRepository.deleteAll();
+    }
+
+    @Test
+    @DisplayName("100개의 요청이 들어온 재고 감소 테스트 - 비관적 락 설정")
+    public void 많은_요청_재고_감소_태스트_성공() throws InterruptedException {
+        int threadCount = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    stockService.decrease(1L, 1L);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+        latch.await();
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+        Assertions.assertEquals(0, stock.getQuantity());
+    }
+}
